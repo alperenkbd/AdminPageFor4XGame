6baf04e50aa428e8ac15d85d5190ac94f3964fe7
Alperen0110 <alperen_kabadayi@hotmail.com>
2024-07-22 17:41:31 +0300

    Add Login screen

---
+++ adminpagefor4xgame.client/src/Screens/SLogin.jsx
@@ -0,0 +1,121 @@
+import { useState } from 'react';
+import PntInput from '../Components/PntInput.jsx';
+import PntButton from '../Components/PntButton.jsx';
+import { FaLock, FaUser } from 'react-icons/fa';
+import styled, { createGlobalStyle } from 'styled-components';
+
+
+
+const SLogin = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const logoUrl = 'https://www.panteon.games/wp-content/uploads/2021/05/news03.png';
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleRightIconClick = () => {
+        alert('Right icon clicked!');
+    };
+
+    const handleButtonClick = () => {
+        alert('Button clicked!');
+    };
+
+    return (
+        <>
+            <GlobalStyle />
+            <AppWrapper>
+                <ImageWrapper>
+                    <Image src={logoUrl} alt="Login Logo" />
+                </ImageWrapper>
+                <Container>
+                    <PntInput
+                        label="Username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                        leftIcon={<FaUser />}
+                        onRightIconClick={handleRightIconClick}
+                    />
+                    <PntInput
+                        label="Password"
+                        type="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        leftIcon={<FaLock />}
+                        onRightIconClick={handleRightIconClick}
+                    />
+                    <PntButton label="Login" onClick={handleButtonClick} />
+                    <SignUp href="/signup">Don't have an account? Sign Up</SignUp>
+                </Container>
+            </AppWrapper>
+        </>
+    );
+};
+
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden; 
+  }
+`;
+
+const AppWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(180deg, #F8EDED, #FF8225);
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  width: 100%; 
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Arial, sans-serif;
+  padding: 60px;
+  border: 2px solid #454545;
+  background: transparent;
+  border-radius: 3px;
+`;
+
+const ImageWrapper = styled.div`
+  position: absolute;
+  top: 22%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 1;
+`;
+
+const Image = styled.img`
+  width: 200px;
+  height: auto;
+  border-radius: 5px;
+`;
+
+const SignUp = styled.a`
+  color: #454545;
+  margin-top: 30px;
+  transition: color 0.3s;
+  text-decoration: underline;
+  cursor: pointer;
+
+  &:hover {
+    color: #FFFFFF;
+  }
+`;
+
+export default SLogin;
