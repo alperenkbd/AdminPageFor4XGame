30dc2f04bbee3868905b3e95ab4447e7b462e716
Alperen0110 <alperen_kabadayi@hotmail.com>
2024-07-23 15:54:19 +0300

    Add Data Layer that contains models and dbContext

---
+++ AdminPageFor4XGame.Server/Data/ApplicationDbContext .cs
@@ -0,0 +1,28 @@
+﻿using AdminPageFor4XGame.Server.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Design;
+
+public class ApplicationDbContext : IdentityDbContext<User>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
+}
+
+
+public class YourDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseMySql("Server=127.0.0.1;Database=panteongames;User=root;Password=Doshuhq.1967", new MySqlServerVersion(new Version(8, 0, 21)));
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
