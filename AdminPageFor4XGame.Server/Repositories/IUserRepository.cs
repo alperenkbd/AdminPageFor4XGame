776153746845709fb4fd5f3333f6a3808de7528a
Alperen0110 <alperen_kabadayi@hotmail.com>
2024-07-23 15:57:04 +0300

    Add Repository Layer

---
+++ AdminPageFor4XGame.Server/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using AdminPageFor4XGame.Server.Data.Models;
+
+namespace AdminPageFor4XGame.Server.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(int id);
+        Task<User> GetUserByUsernameAsync(string username);
+        Task AddUserAsync(User user);
+    }
+}
