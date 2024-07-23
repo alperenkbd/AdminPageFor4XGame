bf895d8ceb2d53e6fc378bb25d0b1fe500d6278e
Alperen0110 <alperen_kabadayi@hotmail.com>
2024-07-23 20:45:19 +0300

    Add PntDataTable

---
+++ adminpagefor4xgame.client/src/Components/PntDataTable.jsx
@@ -0,0 +1,38 @@
+//if more datatable and more configuration need on datatable will be necessary on project, might add materialUi
+
+import React from 'react';
+import DataGrid from 'react-data-grid';
+import 'react-data-grid/lib/styles.css';
+import styled from 'styled-components';
+
+
+
+//const exampleColumns = [
+//    { key: 'id', name: 'ID' },
+//    { key: 'name', name: 'Name' },
+//    { key: 'age', name: 'Age' },
+//];
+
+//const exampleRows = [
+//    { id: 1, name: 'cemil', age: 28 },
+//    { id: 2, name: 'suat', age: 34 },
+//    { id: 3, name: 'orhan', age: 45 }
+//    { id: 4, name: 'yusuf', age: 51 }
+//];
+
+const MyDataGrid = (props) => {
+
+    const { columns, rows, header,customComponent } = props;
+
+    return (
+        <>
+            <h2>{header}</h2>
+            <>{customComponent}</>
+            <DataGrid  columns={columns} rows={rows}  />
+        </>
+    );
+};
+
+
+
+export default MyDataGrid;
